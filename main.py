cd02d5331406b43648aa087440229c17b4c29668
aalvarez11 <aalvarez1@email.arizona.edu>
2025-02-07 22:56:05 -0700

    refactor to one line method call

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext.commands import Bot
 from discord import Intents
 from dotenv import load_dotenv
 import requests
+from twitfix import fix_tweets
 
 # Load Token from env
 load_dotenv()
@@ -28,49 +29,7 @@ async def on_message(msg):
     if msg.author == bot.user:
         return
 
-    botResponse = f'Your friend, {msg.author.display_name}, wanted to share this with you!\n'
-    
-    msgLowered = msg.content.lower()
-
-    # handle multiple line inputs
-    msgContent = msgLowered.splitlines()
-
-    # check if the message is a twitter link
-    twitUrls = ['//twitter.com','//x.com','//fixupx.com','//fixvx.com','//vxtwitter.com','//fxtwitter.com','//girlcockx.com']
-
-    for msgLine in msgContent:
-        for url in twitUrls:
-            if url in msgLine:
-                # check if the tweet has media, then if it is video
-                checkUrl = msgLine.replace(url, '//api.vxtwitter.com')
-                try:
-                    vxApiResponse = requests.get(checkUrl)
-                    vxApiResponse.raise_for_status()
-                    responseData = vxApiResponse.json()
-
-                    if (responseData["hasMedia"] and (responseData["media_extended"][0]["type"] == 'video' or responseData["media_extended"][0]["type"] == 'gif')):
-                        await msg.channel.send(botResponse + responseData["media_extended"][0]["url"])
-                        
-                        # check for text in media tweet
-                        if (responseData["text"]):
-                            responseText = responseData["text"]
-                            # remove quote tweet links
-                            if('https://t.co/' in responseText): responseText = responseText.split('https://t.co/')[0]
-                            # do not send empty strings    
-                            if(responseText != ''): await msg.channel.send(f'\n\"' + responseText + f'\"')
-                    else:
-                        # replace the url with the cancel url
-                        newUrl = msgLine.replace(url, '//xcancel.com')
-
-                        # output a message
-                        await msg.channel.send(botResponse + newUrl)
-
-                except requests.exceptions.HTTPError as e:
-                    raise SystemExit(e)
-
-                # don't delete original until all lines have been parsed
-                if (msgLine == msgContent[len(msgContent)-1]):
-                    await msg.delete()
+    fix_tweets(msg)
 
 # Load token on startup
 bot.run(token=DISCORD_TOKEN)
