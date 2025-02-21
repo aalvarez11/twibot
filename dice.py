01bab35b882ec8c0a235607bc896307bbe641baf
aalvarez11 <aalvarez1@email.arizona.edu>
2025-02-20 23:02:14 -0700

    add special character validation, change modifier check

--- dice.py
+++ dice.py
@@ -5,14 +5,17 @@ async def roll_die(num_of_sides: int):
     return randint(1, num_of_sides)
 
 async def parse_roll_input(msg: str):
+    # only simple rolls allowed
+    if re.search(r'[^0-9a-zA-Z\+-]+', msg) is not None: 
+        raise ValueError('Illegal character found. Please enter a roll without using *, /, !, @, #, $, %, etc.')
     # trim excess characters
     cropped_msg = re.sub(r'[^\d+\-*\/d]', '', msg)
     # get the modifier portion of the command, if any
     roll_modifier = re.search(r'([\+|-]+\d+$)', cropped_msg).group() if re.search(r'([\+|-]+\d+$)', cropped_msg) else ''
     # make a list with sets matching xdy format where x & y are numbers
-    rolls = re.split(r'(\d+d\d+)', cropped_msg)
+    rolls = re.split(r'(\d+d\d+)[^0-9a-zA-Z]+', cropped_msg)
     # remove the modifier from the list
-    if roll_modifier in rolls: rolls.remove(roll_modifier)
+    if roll_modifier != '': rolls.pop()
     rolls.insert(0, roll_modifier)
     # remove empty strings from the list
     rolls[:] = [i for i in rolls if i != '']
@@ -25,7 +28,10 @@ async def parse_roll_input(msg: str):
 async def roll_dice(msg: str):
     if not msg: return 'Hey! You forgot your message! Try again.'
 
-    parsed_roll = parse_roll_input(msg)
+    try:
+        parsed_roll = parse_roll_input(msg)
+    except ValueError as e:
+        return f'Sorry! There was something wrong in your message: {e}'
 
     dice_count = sides = roll_modifier = 0
     
