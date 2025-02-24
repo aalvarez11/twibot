eba4da56b87f70d4a6d1c1eac0196a67c639a008
aalvarez11 <aalvarez1@email.arizona.edu>
2025-02-24 12:35:41 -0700

    add section comments, roll dice tests

--- test_dice.py
+++ test_dice.py
@@ -3,6 +3,9 @@ import asyncio
 import pytest_asyncio
 from dice import parse_roll_input, roll_dice
 
+# =========================================================
+# Testing for parse_roll_input()
+# =========================================================
 @pytest.mark.asyncio
 async def test_parsing_empty_roll():
     test_roll = await parse_roll_input('')
@@ -33,4 +36,32 @@ async def test_parsing_roll_value_error():
     with pytest.raises(ValueError) as excinfo:
         test_roll = await parse_roll_input('4d6*2d10-1')
         assert test_roll == ['-1','4d6','2d10']
-    assert str(excinfo.value) == 'Illegal character found. Please enter a roll without using *, /, !, @, #, $, %, etc.'
+    assert str(excinfo.value) == ('Illegal character found. '
+                                  'Please enter a roll without '
+                                  'using *, /, !, @, #, $, %, etc.')
+
+# =========================================================
+# Testing for roll_dice()
+# =========================================================
+@pytest.mark.asyncio
+async def test_empty_message():
+    test_outcome = await roll_dice('')
+    assert test_outcome == 'Hey! You forgot your message! Try again.'
+
+@pytest.mark.asyncio
+async def test_empty_space_message():
+    test_outcome = await roll_dice('     ')
+    assert test_outcome == ('Sorry! There was something wrong in '
+                            'your message: Illegal character found. '
+                            'Please enter a roll without using '
+                            '*, /, !, @, #, $, %, etc.')
+
+@pytest.mark.asyncio
+async def test_invalid_message_with_symbols():
+    test_outcome = await roll_dice('#$!VNDads')
+    assert test_outcome == ('Sorry! There was something wrong in '
+                            'your message: Illegal character found. '
+                            'Please enter a roll without using '
+                            '*, /, !, @, #, $, %, etc.')
+    
+    
